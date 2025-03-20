0949b68c3ff0e3f9dee15abf6e26d7829c288e15
immortalrover <immortalrover@protonmail.com>
2025-03-21 00:03:48 +0800

    add shortcuts for neotree plugin

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -3,14 +3,27 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
-    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
   },
-  lazy = false, -- neo-tree will lazily load itself
+  lazy = true,
   ---@module "neo-tree"
   ---@type neotree.Config?
   opts = {
-    -- fill any relevant options here
+  },
+  keys = {
+    {
+      "<leader>e",
+      ":Neotree toggle reveal_force_cwd<cr>\
+      :set relativenumber<CR>",
+      desc = "打开文件树",
+    },
+    {
+      "<leader>gs",
+      ":Neotree close<cr>\
+      :Neotree float git_status<cr>\
+      :set relativenumber<CR>",
+      desc = "Show the output of `git status` in a tree layout.",
+    },
   },
 }
